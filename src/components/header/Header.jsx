0cc4f078d868bcae53ac27fc8b9af4c4635a7866
kavishwa-bhashitha <kaveebhashiofficial@gmail.com>
2023-02-06 14:19:30 +0200

    Header Styling done with socials

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,29 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 // import headerImg from '../../assets/kvshw.png'
 import './header.css';
 import GitHub from '../../assets/github 1.png'
+import GitHub2 from '../../assets/github 2.png'
 import LinkedIn from '../../assets/linkedin 1.png'
+import LinkedIn2 from '../../assets/linkedin 2.png'
 import Facebook from '../../assets/facebook 1.png'
+import Facebook2 from '../../assets/facebook 2.png'
 import Twitter from '../../assets/twitter 2.png'
+import Twitter2 from '../../assets/twitter 3.png'
+
+const useImageChangeOnHover = (defaultSource, hoverSource) => {
+    const [source, setSource] = useState(defaultSource);
+
+    const onMouseOver = () => setSource(hoverSource);
+    const onMouseOut = () => setSource(defaultSource);
+
+    return {
+        source,
+        onMouseOver,
+        onMouseOut,
+    };
+};
+
+const ImageChangeOnHover = ({ defaultSource, hoverSource, link }) => {
+    const { source, onMouseOver, onMouseOut } = useImageChangeOnHover(defaultSource, hoverSource);
+
+    return (
+        <a href={link} target="_blank" rel="noopener noreferrer">
+            <img className='social_icons'
+                src={source}
+                onMouseOver={onMouseOver}
+                onMouseOut={onMouseOut}
+                alt="Image change on hover"
+            />
+        </a>
+    );
+};
 
 const Header = () => {
+
     return (
         <div className='kvshw__header' style={{ backgroundImage: 'url(' + require('../../assets/kvshw.png') + ')', height: "90vh", }} >
             <div className='kvshw__header_inner-div'  >
                 <div className='kvshw__header_inner-div_left'>
                     <h1>Kavishwa Wendakoon</h1>
                     <h2>Welcome to my Portfolio</h2>
-                    <p>My mission is to invoke a change in
-                        society using the knowledge I gained throughout
-                        last few years </p>
-                    <p className='contact_btn'><a href='#'>Contact me &gt; </a></p>
+                    <p>My mission is to invoke a change insociety using the knowledge I gained throughout last few years </p>
+                    <h4 className='contact_btn'><a href='#'>Contact me &gt; </a></h4>
                 </div>
                 <div className='kvshw__header_inner-div_right'>
                     <div className='kvshw__header_inner-div_social'>
-                        <img className='social_icons' src={GitHub} alt="" srcset="" />
-                        <img className='social_icons' src={LinkedIn} alt="" srcset="" />
-                        <img className='social_icons' src={Facebook} alt="" srcset="" />
-                        <img className='social_icons' src={Twitter} alt="" srcset="" />
+                        <ImageChangeOnHover defaultSource={GitHub} hoverSource={GitHub2} link={"https://github.com/kvshw"} />
+                        <ImageChangeOnHover defaultSource={LinkedIn} hoverSource={LinkedIn2} link={"https://www.linkedin.com/in/kavishwa-bhashitha/"} />
+                        <ImageChangeOnHover defaultSource={Facebook} hoverSource={Facebook2} link={"https://www.facebook.com/kavishwa.bhashitha"} />
+                        <ImageChangeOnHover defaultSource={Twitter} hoverSource={Twitter2} link={"https://twitter.com/kvshw_"} />
                     </div>
                 </div>
             </div>
