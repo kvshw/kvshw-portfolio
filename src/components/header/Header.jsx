b877f55d15620336b59b63e56d6c62265d4aa4b2
kavishwa-bhashitha <kaveebhashiofficial@gmail.com>
2023-02-08 20:26:58 +0200

    Roadmap added.

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -49,12 +49,32 @@ const Header = () => {
                     <p>My mission is to invoke a change in society using the knowledge I gained throughout last few years </p>
                     <h4 className='contact_btn'><a href='#'>About me &gt; </a></h4>
                 </div>
-                <div className='kvshw__header_inner-div_right'>
-                    <div className='kvshw__header_inner-div_social'>
-                        <ImageChangeOnHover defaultSource={GitHub} hoverSource={GitHub2} link={"https://github.com/kvshw"} />
-                        <ImageChangeOnHover defaultSource={LinkedIn} hoverSource={LinkedIn2} link={"https://www.linkedin.com/in/kavishwa-bhashitha/"} />
-                        <ImageChangeOnHover defaultSource={Facebook} hoverSource={Facebook2} link={"https://www.facebook.com/kavishwa.bhashitha"} />
-                        <ImageChangeOnHover defaultSource={Twitter} hoverSource={Twitter2} link={"https://twitter.com/kvshw_"} />
+                <div className='right__main'>
+                    <div className='upcoming__features'>
+                        <h3>Upcoming Additions</h3>
+                        <ul>
+
+                            <li>--&gt; GitHub + Netlify &#9989;</li>
+                            <li>--&gt; Navigation &#9989;</li>
+                            <li>--&gt; Homepage &#9989;</li>
+                            <li>--&gt; Cursor change &#9989;</li>
+                            <li>--&gt; Resume Downloading <span className='in-progress'>in progress</span></li>
+                            <li>--&gt; About Section</li>
+                            <li>--&gt; Portfolio Section</li>
+                            <li>--&gt; Blog with Prisma</li>
+                            <li>--&gt; Contact me Section</li>
+                            <li>--&gt; Internal Routing </li>
+                            <li>--&gt; Horizontal Scrolling & Parallex</li>
+                            <li>--&gt; Admin dashboard/CRM </li>
+                        </ul>
+                    </div>
+                    <div className='kvshw__header_inner-div_right'>
+                        <div className='kvshw__header_inner-div_social'>
+                            <ImageChangeOnHover defaultSource={GitHub} hoverSource={GitHub2} link={"https://github.com/kvshw"} />
+                            <ImageChangeOnHover defaultSource={LinkedIn} hoverSource={LinkedIn2} link={"https://www.linkedin.com/in/kavishwa-bhashitha/"} />
+                            <ImageChangeOnHover defaultSource={Facebook} hoverSource={Facebook2} link={"https://www.facebook.com/kavishwa.bhashitha"} />
+                            <ImageChangeOnHover defaultSource={Twitter} hoverSource={Twitter2} link={"https://twitter.com/kvshw_"} />
+                        </div>
                     </div>
                 </div>
             </div>
