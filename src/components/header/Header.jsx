c8d6f0ba634fa5dc1317a47cd463fe9f149b454b
kavishwa-bhashitha <kaveebhashiofficial@gmail.com>
2023-02-10 23:49:01 +0200

    tick added

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -59,7 +59,7 @@ const Header = () => {
                             <li>--&gt; Homepage &#9989;</li>
                             <li>--&gt; Cursor change &#9989;</li>
                             <li>--&gt; Resume Downloading &#9989;</li>
-                            <li>--&gt; About Section </li>
+                            <li>--&gt; About Section &#9989;</li>
                             <li>--&gt; Portfolio Section <span className='in-progress'>in progress</span></li>
                             <li>--&gt; Blog with Prisma</li>
                             <li>--&gt; Contact me Section</li>
